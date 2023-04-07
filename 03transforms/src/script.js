1b22269d993ee5030780ece783b8a3c184fe09ac
hakbar0 <haseeb2000@hotmail.com>
2023-04-07 15:43:42 +0100

    able to rotate

--- 03transforms/src/script.js
+++ 03transforms/src/script.js
@@ -9,12 +9,24 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.y = 0;
-mesh.position.x = -1;
-scene.add(mesh);
+const group = new THREE.Group();
+group.rotation.y = 7;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "red" })
+);
+
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "green" })
+);
+
+group.add(cube2);
+cube2.position.x -= 2;
 
 //axes helper
 const axesHelper = new THREE.AxesHelper(3);
@@ -36,16 +48,6 @@ camera.position.z = 3;
 camera.position.y = 1;
 camera.position.x = 1;
 scene.add(camera);
-
-//Scale
-mesh.scale.x = 2;
-mesh.scale.y = 0.5;
-mesh.scale.z = 0.5;
-
-//rotation
-mesh.rotation.reorder("YXZ");
-mesh.rotation.y = Math.PI * 0.25;
-mesh.rotation.x = Math.PI * 0.25;
 
 /**
  * Renderer
