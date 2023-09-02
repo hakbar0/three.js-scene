a5997b3ab1d6699b98aa3f846386f85bc48918c5
hakbar0 <haseeb2000@hotmail.com>
2023-09-02 17:03:49 +0100

    created build

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -1,16 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import * as dat from "lil-gui";
 import { FontLoader } from "three/addons/loaders/FontLoader.js";
 import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
-
-THREE.ColorManagement.enabled = false;
-
-/**
- * Base
- */
-// Debug
-const gui = new dat.GUI();
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -19,21 +10,20 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper);
+// scene.add(axesHelper);
 
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
 const matcapTexture = textureLoader.load("/textures/matcaps/9.png");
-console.log(matcapTexture);
 
 const fontLoader = new FontLoader();
 
 fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
   const textGeometry = new TextGeometry("Do the Work ", {
     font: font,
-    size: 0.5,
+    size: 0.6,
     height: 0.2,
     curveSegments: 5,
     bevelEnabled: true,
@@ -43,42 +33,55 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
     bevelSegments: 4,
   });
 
-  // textGeometry.computeBoundingBox();
-  // textGeometry.translate(
-  //   -textGeometry.boundingBox.max.x * 0.5,
-  //   -textGeometry.boundingBox.max.y * 0.5,
-  //   -textGeometry.boundingBox.max.z * 0.5
-  // );
   textGeometry.center();
 
   const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
-  // textMaterial.wireframe = true;
   const text = new THREE.Mesh(textGeometry, material);
   scene.add(text);
 
   const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45);
 
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 700; i++) {
     const donut = new THREE.Mesh(donutGeometry, material);
 
-    donut.position.x = (Math.random() - 0.5) * 10;
-    donut.position.y = (Math.random() - 0.5) * 10;
-    donut.position.z = (Math.random() - 0.5) * 10;
+    donut.position.x = (Math.random() - 0.5) * 20; // increased from 15 to 20
+    donut.position.y = (Math.random() - 0.5) * 20; // increased from 15 to 20
+    donut.position.z = (Math.random() - 0.5) * 20; // increased from 15 to 20
 
     donut.rotation.x = Math.random() * Math.PI;
     donut.rotation.y = Math.random() * Math.PI;
 
-    const scale = Math.random();
-    donut.scale.x = scale;
-    donut.scale.y = scale;
-    donut.scale.z = scale;
+    const scale = 0.5 * Math.random(); // reduced max scale to 0.5 from 1
+    donut.scale.set(scale, scale, scale);
 
     scene.add(donut);
   }
 });
-/**
- * Object
- */
+
+const starGeometry = new THREE.BufferGeometry();
+const starVertices = [];
+
+for (let i = 0; i < 5000; i++) {
+  const x = (Math.random() - 0.5) * 200; // spread out more for stars
+  const y = (Math.random() - 0.5) * 200;
+  const z = (Math.random() - 0.5) * 200;
+  starVertices.push(x, y, z);
+}
+
+starGeometry.setAttribute(
+  "position",
+  new THREE.Float32BufferAttribute(starVertices, 3)
+);
+
+const starMaterial = new THREE.PointsMaterial({
+  color: 0xffffff, // white stars
+  size: 0.25, // adjust the size if stars seem too big/small
+  transparent: true,
+  opacity: 0.8,
+});
+
+const stars = new THREE.Points(starGeometry, starMaterial);
+scene.add(stars);
 
 /**
  * Sizes
@@ -89,15 +92,12 @@ const sizes = {
 };
 
 window.addEventListener("resize", () => {
-  // Update sizes
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
 
-  // Update camera
   camera.aspect = sizes.width / sizes.height;
   camera.updateProjectionMatrix();
 
-  // Update renderer
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
@@ -105,7 +105,6 @@ window.addEventListener("resize", () => {
 /**
  * Camera
  */
-// Base camera
 const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
@@ -124,10 +123,7 @@ controls.enableDamping = true;
 /**
  * Renderer
  */
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
-renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
@@ -137,15 +133,8 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 const clock = new THREE.Clock();
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-
-  // Update controls
   controls.update();
-
-  // Render
   renderer.render(scene, camera);
-
-  // Call tick again on the next frame
   window.requestAnimationFrame(tick);
 };
 
