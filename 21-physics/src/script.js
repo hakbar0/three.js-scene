2a94ae4faa66aa9620c03c60eb1475758f95175e
hakbar0 <haseeb2000@hotmail.com>
2023-09-03 19:09:41 +0100

    more physics

--- 21-physics/src/script.js
+++ 21-physics/src/script.js
@@ -61,6 +61,10 @@ const sphereBody = new CANNON.Body({
   position: new CANNON.Vec3(0, 3, 0),
   shape: sphereShape,
 });
+sphereBody.applyLocalForce(
+  new CANNON.Vec3(150, 0, 0),
+  new CANNON.Vec3(0, 0, 0)
+);
 world.addBody(sphereBody);
 
 // Floor
@@ -182,6 +186,9 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
   const deltaTime = elapsedTime - oldElaspedTime;
   oldElaspedTime = elapsedTime;
+
+  // Update physics
+  sphereBody.applyForce(new CANNON.Vec3(-0.5, 0, 0), sphereBody.position);
 
   // Update Physcs World
   world.step(1 / 60, deltaTime, 3);
