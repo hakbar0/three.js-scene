978889350492016fdd158dc28b8b5c16752c6606
hakbar0 <haseeb2000@hotmail.com>
2023-09-03 12:38:32 +0100

    added partiles

--- 18-particles/src/script.js
+++ 18-particles/src/script.js
@@ -21,6 +21,33 @@ const scene = new THREE.Scene();
  */
 const textureLoader = new THREE.TextureLoader();
 
+// Geometry
+const particlesMaterial = new THREE.PointsMaterial();
+particlesMaterial.size = 0.02;
+particlesMaterial.sizeAttenuation = true;
+
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 500;
+
+const positions = new Float32Array(count * 3); // Multiply by 3 because each position is composed of 3 values (x, y, z)
+
+for (
+  let i = 0;
+  i < count * 3;
+  i++ // Multiply by 3 for same reason
+) {
+  positions[i] = (Math.random() - 0.5) * 10; // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+}
+
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
+); // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
+
+// points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
+
 /**
  * Sizes
  */
