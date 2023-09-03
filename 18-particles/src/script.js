542f26044eaa6244a753dc52fcc0bd254288ed22
hakbar0 <haseeb2000@hotmail.com>
2023-09-03 13:01:41 +0100

    particle colours

--- 18-particles/src/script.js
+++ 18-particles/src/script.js
@@ -20,30 +20,44 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load("/textures/particles/9.png");
+
+// ...
 
 // Geometry
 const particlesMaterial = new THREE.PointsMaterial();
-particlesMaterial.size = 0.02;
+particlesMaterial.size = 0.1;
 particlesMaterial.sizeAttenuation = true;
 
-const particlesGeometry = new THREE.BufferGeometry();
-const count = 500;
+// particlesMaterial.map = particleTexture
+particlesMaterial.transparent = true;
+particlesMaterial.alphaMap = particleTexture;
+// // particlesMaterial.alphaTest = 0.0001;
+// particlesMaterial.depthTest = false;
+particlesMaterial.depthWrite = false;
+particlesMaterial.blending = THREE.AdditiveBlending;
+particlesMaterial.vertexColors = true;
 
-const positions = new Float32Array(count * 3); // Multiply by 3 because each position is composed of 3 values (x, y, z)
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 6000;
+
+const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
 
 for (
   let i = 0;
   i < count * 3;
   i++ // Multiply by 3 for same reason
 ) {
-  positions[i] = (Math.random() - 0.5) * 10; // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+  positions[i] = (Math.random() - 0.5) * 10;
+  colors[i] = Math.random();
 }
 
 particlesGeometry.setAttribute(
   "position",
   new THREE.BufferAttribute(positions, 3)
-); // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
-
+);
+particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
 // points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial);
 scene.add(particles);
